caf52d4cc8d6ecdc63f933be3cfae4481b6f25ed
Mickael LY <mickael.ly.fr@gmail.com>
2022-03-19 20:28:28 +0100

    init markdown reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import markdownReducer from '../features/counter/markdown';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    markdown: markdownReducer
   },
 });
